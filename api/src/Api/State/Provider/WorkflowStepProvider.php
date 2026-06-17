f764f0e32929b5106d6ad99bd3244582149138ff
BatMaxou <90443919+BatMaxou@users.noreply.github.com>
2026-06-17 13:14:15 +0200

    :sparkles: Serialization workflow (#27)

--- api/src/Api/State/Provider/WorkflowStepProvider.php
+++ api/src/Api/State/Provider/WorkflowStepProvider.php
@@ -18,7 +18,6 @@ final class WorkflowStepProvider implements ProviderInterface
     public function provide(Operation $operation, array $uriVariables = [], array $context = []): WorkflowStepResult
     {
         $choice = $this->choiceRepository->find($uriVariables['choiceId']);
-
         if (null === $choice) {
             throw new NotFoundHttpException('Choice not found.');
         }
@@ -28,7 +27,6 @@ final class WorkflowStepProvider implements ProviderInterface
         }
 
         $pass = $choice->getRecommendedPass();
-
         if (null === $pass) {
             throw new \LogicException('Choice has neither nextQuestion nor recommendedPass.');
         }
